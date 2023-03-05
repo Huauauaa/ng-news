3803092d951bdf60e4bac1e3b82831535752728b
Harvey <harvey0379@163.com>
2023-03-05 22:09:46 +0800

    build: copy-webpack-plugin Huauauaa/cheat-sheet#66

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,4 +1,16 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = (config) => {
-    console.log(config);
+    config.plugins.push(
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, 'src/static'),
+                    to: path.join(__dirname, 'dist/static'),
+                },
+            ],
+        }),
+    );
     return config;
 };
